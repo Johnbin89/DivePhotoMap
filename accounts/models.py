20c34572862e198b27a73b3699f2008ee7db27f4
Johnbin89 <46113530+Johnbin89@users.noreply.github.com>
2024-06-10 14:18:51 +0300

    initial user setup

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,9 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser
 # Create your models here.
+
+
+#https://docs.djangoproject.com/en/5.0/topics/auth/customizing/#using-a-custom-user-model-when-starting-a-project
+class User(AbstractUser):
+    photos_uploaded = models.PositiveIntegerField(verbose_name='photos_uploaded', default=0)
+    likes_earned = models.PositiveIntegerField(verbose_name='likes_earned', default=0)
---
+++ accounts/serializers.py
@@ -0,0 +1,7 @@
+from accounts.models import User
+from rest_framework import serializers
+
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = User
+        fields = ['url', 'username', 'email', 'photos_uploaded', 'likes_earned']

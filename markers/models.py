5b156946017d2b911a5d4ed8373a1b325c1a2e79
Johnbin89 <46113530+Johnbin89@users.noreply.github.com>
2024-06-15 01:47:11 +0300

    DecimalField/public Marker

--- markers/models.py
+++ markers/models.py
@@ -3,5 +3,6 @@ from django.db import models
 # Create your models here.
 class Marker(models.Model):
     owner = models.ForeignKey('accounts.User', on_delete=models.CASCADE)
-    posLat = models.DecimalField(verbose_name='Marker Latitude')
-    posLng = models.DecimalField(verbose_name='Marker Longitude')
+    public = models.BooleanField()
+    posLat = models.DecimalField(verbose_name='Marker Latitude', max_digits=7, decimal_places=5)
+    posLng = models.DecimalField(verbose_name='Marker Longitude', max_digits=7, decimal_places=5)
